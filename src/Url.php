db1f25efa37a5238fefe248c3dc24f6d53dc3d6f
Yuta Nagamiya <y.nagamiya@gmail.com>
2021-02-14 01:32:44 +0900

    docs

--- src/Url.php
+++ src/Url.php
@@ -17,90 +17,90 @@ abstract class Url
     abstract protected function validate(): void;
 
     /**
-     * @param string|UriInterface $uri
+     * @param string|UriInterface $url
      */
-    public static function createBaseUrl($uri): BaseUrl
+    public static function createBaseUrl($url): BaseUrl
     {
-        return new BaseUrl($uri);
+        return new BaseUrl($url);
     }
 
     /**
-     * @param string|UriInterface $uri
+     * @param string|UriInterface $url
      */
-    public static function createRelativeUrl($uri): RelativeUrl
+    public static function createRelativeUrl($url): RelativeUrl
     {
-        return new RelativeUrl($uri);
+        return new RelativeUrl($url);
     }
 
     /**
-     * @param string|UriInterface $uri
+     * @param string|UriInterface $url
      */
-    public static function createFullUrl($uri): FullUrl
+    public static function createFullUrl($url): FullUrl
     {
-        return new FullUrl($uri);
+        return new FullUrl($url);
     }
 
     /**
-     * @param string|UriInterface $uri
+     * @param string|UriInterface $url
      * @throws InvalidArgumentException
      */
-    public static function createRequestUrl($uri, ?BaseUrl $baseUrl = null): FullUrl
+    public static function createRequestUrl($url, ?BaseUrl $baseUrl = null): FullUrl
     {
         if (\is_null($baseUrl)) {
             try {
-                return new FullUrl($uri);
+                return new FullUrl($url);
             } catch (InvalidArgumentException $e) {
                 // no-op
             }
             throw new InvalidArgumentException(\sprintf(
                 'The request URL "%s" must be the full URL because the base URL is not specifided.',
-                (string) $uri
+                (string) $url
             ), 0, $e);
         } else {
             try {
-                return $baseUrl->createFullUrlWithRelativeUrl(new RelativeUrl($uri));
+                return $baseUrl->createFullUrlWithRelativeUrl(new RelativeUrl($url));
             } catch (InvalidArgumentException $e) {
                 // no-op
             }
             throw new InvalidArgumentException(\sprintf(
                 'The request URL "%s" must be the relative URL because the base URL is specified.',
-                (string) $uri
+                (string) $url
             ), 0, $e);
         }
     }
 
     /**
-     * @param string|UriInterface $uri
+     * @param string|UriInterface $url
      * @throws InvalidArgumentException
      * @return FullUrl|RelativeUrl
      */
-    public static function createDestinationUrl($uri, ?BaseUrl $baseUrl = null): Url
+    public static function createDestinationUrl($url, ?BaseUrl $baseUrl = null): Url
     {
         if (\is_null($baseUrl)) {
             try {
-                return new FullUrl($uri);
+                return new FullUrl($url);
             } catch (InvalidArgumentException $e1) {
                 // no-op
             }
             try {
-                return new RelativeUrl($uri);
+                return new RelativeUrl($url);
             } catch (InvalidArgumentException $e2) {
                 // no-op
             }
             $e = self::withPrevious($e2, $e1);
             throw new InvalidArgumentException(\sprintf(
                 'The destination URL "%s" is invalid.',
-                (string) $uri
+                (string) $url
             ), 0, $e);
         } else {
             try {
-                return $baseUrl->createFullUrlWithRelativeUrl(new RelativeUrl($uri));
+                return $baseUrl->createFullUrlWithRelativeUrl(new RelativeUrl($url));
             } catch (InvalidArgumentException $e) {
                 // no-op
             }
             throw new InvalidArgumentException(\sprintf(
                 'The destination URL "%s" must be the relative URL because the base URL is specified.',
-                (string) $uri
+                (string) $url
             ), 0, $e);
         }
     }
@@ -131,11 +131,11 @@ abstract class Url
     }
 
     /**
-     * @param string|UriInterface|Url $uri
+     * @param string|UriInterface $url
      */
-    protected function __construct($uri)
+    protected function __construct($url)
     {
-        $this->uri = Psr17FactoryDiscovery::findUriFactory()->createUri((string) $uri);
+        $this->uri = Psr17FactoryDiscovery::findUriFactory()->createUri((string) $url);
         $this->validate();
     }
 
