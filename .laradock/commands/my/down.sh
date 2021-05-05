819b9e06a1b6f5441e4f83cce88b2d4f9e005f03
Yuta Nagamiya <y.nagamiya@gmail.com>
2021-05-06 00:10:26 +0900

    rm

--- .laradock/commands/my/down.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -Ceuo pipefail
-
-local NAME='my:down'
-local DESCRIPTION='Shut down my development environment'
-
-handle() {
-  docker-compose stop
-}
--- .laradock/commands/my/phive.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -Ceuo pipefail
-
-local NAME='my:phive'
-local DESCRIPTION='Execute a PHIVE command'
-
-handle() {
-  docker-compose exec -u laradock workspace phive --home .laradock/data/phive "$@"
-}
